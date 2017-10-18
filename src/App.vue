4ba3dddd0cfff83f961b2c319ccf0500ef6ea7ac
lxk <2664276518@qq.com>
2017-10-18 17:52:27 +0800

    version 1

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      ...
+
+    </div>
+    <div class="content">
+      ...
+
+    </div>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue';
+
+  export default {
+    components: {
+      'v-header': header
+    }
+  };
+</script>
+
+<style>
+
+</style>
